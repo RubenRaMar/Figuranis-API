4184806a33957a13ad393361801abbb281cf9877
RubenRaMar <rubenfr1@hotmail.com>
2023-05-25 15:57:16 +0200

    Create endpointNotFound middleware

--- src/server/middlewares/errors/errorMiddleware.ts
+++ src/server/middlewares/errors/errorMiddleware.ts
@@ -1,12 +1,25 @@
 import { type Request, type Response } from "express";
 import { type NextFunction } from "express-serve-static-core";
 import createDebug from "debug";
-import type CustomError from "../../../Classes/CustomError/CustomError.js";
+import CustomError from "../../../Classes/CustomError/CustomError.js";
 import chalk from "chalk";
 
 const debug = createDebug(
   "figuranisdb-api:server:middlewares:errors:errorMiddleware"
 );
+
+export const endpointNotFound = (
+  _req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  const statusCode = 404;
+  const message = "Endpoint not found";
+
+  const error = new CustomError(statusCode, message);
+
+  next(error);
+};
 
 export const generalError = (
   error: CustomError,
