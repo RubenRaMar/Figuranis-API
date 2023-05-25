3d4eafda4cfa6a9afa976d6eac92d1e63aaf605a
RubenRaMar <rubenfr1@hotmail.com>
2023-05-25 11:39:37 +0200

    Initialize the server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+import "./loadEnvironments.js";
 import createDebug from "debug";
+import app from "./server/app.js";
+import chalk from "chalk";
 
+const port = process.env.PORT ?? 4000;
 const debug = createDebug("figuranisdb-api:root");
 
-debug("");
+app.listen(port, () => {
+  debug(chalk.green(`Listening on http://localhost:${port}`));
+});
+
+app.use();
