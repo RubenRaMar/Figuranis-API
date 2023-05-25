efe76175dbdbb1b15431aeea5ea16587efcef289
RubenRaMar <rubenfr1@hotmail.com>
2023-05-25 22:33:52 +0200

    Create User model

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,8 @@
 import { type Response } from "express";
 
 export type CustomResponse = Pick<Response, "status" | "json">;
+
+export interface UserCredentials {
+  username: string;
+  password: string;
+}
