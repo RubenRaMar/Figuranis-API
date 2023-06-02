bf13e1e3b96068dbde032d4c6fee7db940b762f9
RubenRaMar <rubenfr1@hotmail.com>
2023-06-02 13:56:01 +0200

    Perform figureRouter router integration test

--- src/mocks/user/userMoks.ts
+++ src/mocks/user/userMoks.ts
@@ -14,3 +14,6 @@ export const userErrorCredientialsMock = {
   ...userCredentialsMock,
   password: "",
 };
+
+export const tokenMock =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDZmYzUwOTEwYzhlOGM1YjE3ZDU0YTciLCJuYW1lIjoiYWRtaW4iLCJpYXQiOjE2ODU3MDUzNDIsImV4cCI6MTY4NTg3ODE0Mn0.A0S4A6QH-Ya5DCtwvVz85dVgZRcWTdlWOVK__O8rajU";
