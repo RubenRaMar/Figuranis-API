1702a5db1211625b5b1af9935231605b673178b3
RubenRaMar <rubenfr1@hotmail.com>
2023-09-24 22:43:15 +0200

    Test registerUser controller and their use cases

---
+++ src/server/controllers/user/__test__/registerUser.test.ts
@@ -0,0 +1,80 @@
+import { Types } from "mongoose";
+import { type UserCredentialsRequest } from "../types";
+import { type NextFunction, type Response } from "express";
+import User from "../../../../database/models/User";
+import {
+  privateMessageList,
+  statusCodeList,
+} from "../../../utils/responseData/responseData";
+import { registerUser } from "../userController";
+import CustomError from "../../../Classes/CustomError/CustomError";
+
+describe("Given a registerUser controller", () => {
+  const userCredentialsMock = {
+    username: "goku",
+    password: "god",
+  };
+
+  const userCredentialsIdMock = {
+    _id: new Types.ObjectId().toString(),
+    ...userCredentialsMock,
+  };
+
+  const req: Pick<UserCredentialsRequest, "body"> = {
+    body: userCredentialsMock,
+  };
+
+  const res: Pick<Response, "status" | "json"> = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  const next = jest.fn();
+
+  describe("When it invoked with de valid credendials", () => {
+    User.create = jest.fn().mockReturnValue(userCredentialsIdMock);
+
+    test("Then it should call the response's method status with a 201", async () => {
+      const expectedStatusCode = statusCodeList.add;
+
+      await registerUser(
+        req as UserCredentialsRequest,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+
+    test("Then it should call the response's method json with the credentials", async () => {
+      const { username: expectedUsername } = userCredentialsMock;
+
+      await registerUser(
+        req as UserCredentialsRequest,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.json).toHaveBeenCalledWith({ user: expectedUsername });
+    });
+  });
+
+  describe("When it invoked with a wrong username and a next function", () => {
+    test("Then it should call the next function", async () => {
+      const expectedError = new CustomError(
+        statusCodeList.badRequest,
+        privateMessageList.registerError
+      );
+
+      User.create = jest.fn().mockRejectedValue(expectedError);
+
+      await registerUser(
+        req as UserCredentialsRequest,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
--- src/server/router/user/userRouter.ts
+++ src/server/router/user/userRouter.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import loginUser from "../../controllers/user/userController.js";
+import {
+  loginUser,
+  registerUser,
+} from "../../controllers/user/userController.js";
 import pathList from "../../utils/path/path.js";
 import { loginValidation } from "../../../schema/loginSchema.js";
 
 const userRouter = Router();
 
 userRouter.post(pathList.login, loginValidation, loginUser);
+userRouter.post(pathList.register, loginValidation, registerUser);
 
 export default userRouter;
